70ecd8a89cfaa51ddbf94fb0b0689a4bae74971d
Bruno Teixeira <bruno_teixeira20@hotmail.com>
2022-07-18 14:59:23 -0300

    user login test

---
+++ backend/tests/Features/app/Http/Controllers/AuthControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Features\app\Http\Controllers;
+
+use App\Models\User;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+  use DatabaseTransactions;
+
+  public function testUserCanLogin()
+  {
+    // prepare
+    $user = User::factory()->create([
+      'email' => 'jhondoe@gmail.com'
+    ]);
+
+    $payload = [
+      'email' => $user->email,
+      'password' => 'password',
+    ];
+
+    // Act
+    $response = $this->post(route('login'), $payload);
+
+    // assert
+    $response->assertResponseStatus(200);
+    $response->seeJsonStructure([
+      'access_token',
+      'token_type',
+      'expires_in',
+      'user' => [
+        'name',
+        'email',
+      ],
+    ]);
+  }
+}
