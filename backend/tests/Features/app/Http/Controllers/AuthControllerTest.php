868c92f53a2723fcf66b616c072b108977b27393
Bruno Teixeira <bruno_teixeira20@hotmail.com>
2022-07-18 15:21:58 -0300

    test if user credentials must be correct to login

--- backend/tests/Features/app/Http/Controllers/AuthControllerTest.php
+++ backend/tests/Features/app/Http/Controllers/AuthControllerTest.php
@@ -37,4 +37,24 @@ class AuthControllerTest extends TestCase
       ],
     ]);
   }
+
+  public function testUserCredentialsMustBeCorrectToLogin()
+  {
+    // prepare
+    $user = User::factory()->create([
+      'email' => 'jhondoe@gmail.com'
+    ]);
+
+    $payload = [
+      'email' => $user->email,
+      'password' => 'password-wrong',
+    ];
+
+    // Act
+    $response = $this->post(route('login'), $payload);
+
+    // assert
+    $response->assertResponseStatus(401);
+    $response->seeJsonStructure(['error']);
+  }
 }
